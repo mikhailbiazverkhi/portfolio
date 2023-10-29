bfb55fdb2fea43b025108e070d02f050f9a6c4cd
Mikhail <mikhail.biazverkhi@gmail.com>
2023-10-29 07:49:47 -0400

    Add New Portfolio Web

---
+++ portfolio-web/js/translation.js
@@ -0,0 +1,236 @@
+const translation = {
+  // burger menu
+  "lang-switcher": {
+    "fr": "en",
+    "en": "fr",
+  },
+  "burger-home": {
+    "fr": "Accueil",
+    "en": "Home",
+  },
+  "burger-about": {
+    "fr": "À propos",
+    "en": "About",
+  },
+  "burger-resume": {
+    "fr": "CV",
+    "en": "Resume",
+  },
+  "burger-portfolio": {
+     "fr": "Portfolio",
+     "en": "Portfolio",
+  },
+  "burger-achievements": {
+    "fr": "Réalisations",
+    "en": "Achievements",
+  },
+  "burger-contact": {
+    "fr": "Contact",
+    "en": "Contact",
+  },
+  // Intro section
+  "hello-world": {
+    "fr": "Bonjour le monde!",
+    "en": "Hello, World!",
+  },
+  "introduce-myself": {
+    "fr": "Je m'appelle <br>Mikhail Biazverkhi.",
+    "en": "I'm <br>Mikhail Biazverkhi.",
+  },
+  "about-me-button": {
+    "fr": "PLUS À PROPOS DE MOI",
+    "en": "MORE ABOUT ME ",
+  },
+  // About section
+  "about-introduce-title": {
+    "fr": "Laissez-moi me présenter",
+    "en": "Let me introduce myself",
+  },
+  "about-text": {
+    "fr": "J'ai accumulé 6 ans d'expérience dans le domaine de l'informatique, ayant occupé des postes d'administrateur système, de testeur en automatisation et de développeur backend. À mon arrivée au Canada, j'ai fait le choix de devenir développeur web pour exprimer ma créativité. Mon parcours varié et mes compétences complémentaires me permettent d'obtenir d'excellents résultats pour satisfaire les clients. Je suis passionné par l'apprentissage continu et l'amélioration, et je suis prêt à sortir de ma zone de confort pour atteindre de nouveaux sommets. J'accorde une grande importance au travail d'équipe et je suis convaincu que notre collaboration sera couronnée de succès.",
+    "en": "I have accumulated 6 years of experience in the IT field, having held positions as a system administrator, automation tester and backend developer. When I arrived in Canada, I chose to become a web developer to express my creativity. My varied background and complementary skills allow me to obtain excellent results to satisfy clients. I am passionate about continuous learning and improvement, and am willing to step out of my comfort zone to reach new heights. I place great importance on teamwork and I am convinced that our collaboration will be crowned with success.",
+  },
+  "about-skills": {
+    "fr": "Compétences",
+    "en": "Skills",
+  },
+  "hire-button": {
+    "fr": "ENGAGEZ-MOI",
+    "en": "HIRE ME",
+  },
+  "download-cv-button": {
+    "fr": "TÉLÉCHARGER LE CV",
+    "en": "DOWNLOAD CV",
+  },
+  // Resume section
+  "resume": {
+    "fr": "CV",
+    "en": "Resume",
+  },
+  "resume-title": {
+    "fr": "Plus de mes références.",
+    "en": "More of my credentials.",
+  },
+  "education": {
+    "fr": "Éducation",
+    "en": "Education",
+  },
+  "university-1": {
+    "fr": "Attestation d’études collégiales - Développement de sites WEB transactionnels, Cégep Saint-Jean-sur-Richelieu, Québec",
+    "en": "College Studies Certificate - Transactional WEB Site Development, Cégep Saint-Jean-sur-Richelieu, Quebec",
+  },
+  "university-2": {
+    "fr": "Diplôme d'ingénieur radio (BAC), Université d'État biélorusse d'informatique et de radio électronique, Minsk, Bélarus",
+    "en": "Radio engineering degree (BAC), Belarusian State University of Informatics and Radio Electronics, Minsk, Belarus",
+  },
+  // Portfolio section
+  "portfolio": {
+    "fr": "portfolio",
+    "en": "portfolio",
+  },
+  "portfolio-title": {
+    "fr": "Découvrez certaines de mes œuvres.",
+    "en": "Check Out Some of My Works.",
+  },
+  "close-button": {
+    "fr": "FERMER",
+    "en": "CLOSE",
+  },
+  "portfolio-project-type-1": {
+    "fr": "Programmation Web côté serveur",
+    "en": "Server Side Web Programming ",
+  },
+  "portfolio-project-type-2": {
+    "fr": "Programmation interactive client-serveur",
+    "en": "Client-server interactive programming",
+  },
+  "portfolio-project-type-3": {
+    "fr": "Programmation Web côté client",
+    "en": "Client-Side Web Programming",
+  },
+  "portfolio-project-type-4": {
+    "fr": "Base de données",
+    "en": "Database",
+  },
+  "portfolio-project-type-5": {
+    "fr": "Éléments d’infographie pour sites Web",
+    "en": "Infographic elements for websites",
+  },
+  "portfolio-project-name-1": {
+    "fr": "API et React",
+    "en": "API and React",
+  },
+  "portfolio-project-description-1": {
+    "fr": "Le propriétaire du Super Club s’est fait voler son idée par une petite compagnie, Instagram. Cependant, la section café était très populaire. Il aimerait que vous transformiez l’application actuelle en React. Vous pouvez laisser tomber tout le reste qui n’avait pas directement rapport aux cafés. Il veut aussi ajouter une section e-commerce pour revendre des produits Nespresso.",
+    "en": "The owner of the Super Club had his idea stolen by a small company, Instagram. However, the coffee section was very popular. He would like you to transform the current application to React. You can leave out everything else that wasn't directly related to cafes. He also wants to add an e-commerce section to resell Nespresso products.",
+  },
+  "portfolio-project-name-2": {
+    "fr": "API RESTful de la plateforme du Blog",
+    "en": "RESTful API of the Blog platform",
+  },
+  "portfolio-project-description-2": {
+    "fr": "Transformer votre application backend existante (Plateforme du Blog vu dans le cours) en une application API RESTful orientée objet.",
+    "en": "Transform your existing backend application (Blog Platform seen in the course) into an object-oriented RESTful API application.",
+  },
+  "portfolio-project-name-3": {
+    "fr": "Application de Streaming Vidéo",
+    "en": "Video Streaming Application",
+  },
+  "portfolio-project-description-3": {
+    "fr": "L’objectif est d’implémenter une plateforme simplifiée de streaming vidéo où les utilisateurs peuvent téléverser et lister toutes les vidéos téléversées en affichant pour chacune le titre, la description ou la catégorie.",
+    "en": "The objective is to implement a simplified video streaming platform where users can upload and list all uploaded videos by displaying the title, description, or category for each.",
+  },
+  "portfolio-project-name-4": {
+    "fr": "Site web - Boutique en ligne",
+    "en": "Website - Online store",
+  },
+  "portfolio-project-description-4": {
+    "fr": "Vous devez créer une page de boutique en ligne (React).",
+    "en": "You need to create an online store page (React).",
+  },
+  "portfolio-project-name-5": {
+    "fr": "Système de gestion de base de données de films avec PHP et MySQL",
+    "en": "Movie Database Management System with PHP and MySQL",
+  },
+  "portfolio-project-description-5": {
+    "fr": "Créer un système simple de gestion de base de données de films en utilisant PHP et MySQL. Ce système doit permettre aux utilisateurs de voir, rechercher, mettre à jour et supprimer des enregistrements de films tout en gérant l'authentification des utilisateurs et le contrôle d'accès.",
+    "en": "Create a simple movie database management system using PHP and MySQL. This system must allow users to view, search, update and delete movie recordings while managing user authentication and access control.",
+  },
+  "portfolio-project-name-6": {
+    "fr": "Site de vente aux enchères (PHP)",
+    "en": "Auction site (PHP)",
+  },
+  "portfolio-project-description-6": {
+    "fr": "Le sujet retenu est la réalisation d’un site de ventes aux enchères de style eBay. Toute personne inscrite sur le site peut participer aux enchères. Les utilisateurs du site devront donc d'abord s'inscrire avant de pouvoir vendre ou acheter des objets.",
+    "en": "The subject chosen is the creation of an eBay-style auction site. Anyone registered on the site can participate in the auction. Users of the site will therefore first have to register before being able to sell or buy items.",
+  },
+  "portfolio-project-name-7": {
+    "fr": "Maquette du site HabitationPlus (AdobeXD et Photoshop)",
+    "en": "Model of the HabitationPlus site (AdobeXD and Photoshop)",
+  },
+  "portfolio-project-description-7": {
+    "fr": "Création d’une maquette en haute résolution.",
+    "en": "Creation of a high-resolution model.",
+  },
+  "portfolio-project-name-8": {
+    "fr": "FoodieShare - Plateforme d'échange de repas étudiants",
+    "en": "FoodieShare - Student meal exchange platform",
+  },
+  "portfolio-project-description-8": {
+    "fr": "Ce projet permettra aux étudiants de partager leurs expériences culinaires, de découvrir de nouveaux plats et de prendre des décisions éclairées lorsqu'ils choisissent où manger sur le campus ou à proximité.",
+    "en": "This project will allow students to share their culinary experiences, discover new dishes and make informed decisions when choosing where to eat on or near campus.",
+  },
+  "portfolio-project-name-9": {
+    "fr": "Sportify (JavaScript)",
+    "en": "Sportify (JavaScript)",
+  },
+  "portfolio-project-description-9": {
+    "fr": "Connaissant vos nouvelles habiletés en développement logiciel, une personne de votre entourage vous approche pour vous parler d’une idée de Startup récemment obtenue. Sportify, comme il l’appelle, est une plateforme ayant pour l’objectif la création de listes de lecture de musique lors d’entrainements ou d’efforts physiques.",
+    "en": "Knowing your new skills in software development, someone close to you approaches you to tell you about a recently obtained Startup idea. Sportify, as it calls it, is a platform aimed at creating music playlists during training or physical efforts.",
+  },
+  "portfolio-project-name-10": {
+    "fr": "Van Gore (HTML et CSS)",
+    "en": "Van Gore (HTML et CSS)",
+  },
+  "portfolio-project-description-10": {
+    "fr": "L’artiste Vincent Van Gore, spécialisé dans l’horreur, vous approche puisqu’il aurait besoin d’aide pour développer son site vitrine. Outre que de peindre des toiles et de créer d’autres œuvres d’arts variés, Van Gore possède aussi sa petite école où il donne des cours privés et des cours de groupes à des gens de la communauté.",
+    "en": "The artist Vincent Van Gore, specialized in horror, is approaching you because he needs help developing his showcase site. In addition to painting canvases and creating other various works of art, Van Gore also has his small school where he gives private lessons and group lessons to people in the community.",
+  },
+  "portfolio-project-name-11": {
+    "fr": "Site Web «Voiture de luxe»",
+    "en": "«Luxury Car» Website",
+  },
+  "portfolio-project-description-11": {
+    "fr": "",
+    "en": "",
+  },
+  // Contact section
+  "contact": {
+    "fr": "Contact",
+    "en": "Contact",
+  },
+  "contact-title": {
+    "fr": "J'aimerais avoir de vos nouvelles.",
+    "en": "I'd Love To Hear From You.",
+  },
+  "submit-button": {
+    "fr": "SOUMETTRE",
+    "en": "SUBMIT",
+  },
+  "form-name": {
+    "fr": "Nom",
+    "en": "Name",
+  },
+  "form-email": {
+    "fr": "E-mail",
+    "en": "Email",
+  },
+  "form-subject": {
+    "fr": "Sujette",
+    "en": "Subject",
+  },
+  "form-message": {
+    "fr": "Message",
+    "en": "Message",
+  },
+}
